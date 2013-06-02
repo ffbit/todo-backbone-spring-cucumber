d50c2cc0e1c07b426c62c7ca3dfe97dbff245d4e
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-06-02 16:36:49 +0300

    Fake delete for todo

--- src/main/java/com/ffbit/todo/web/ToDoController.java
+++ src/main/java/com/ffbit/todo/web/ToDoController.java
@@ -2,14 +2,17 @@ package com.ffbit.todo.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/todos")
@@ -32,9 +35,17 @@ public class ToDoController {
 
     @RequestMapping(value = "{id}", method = RequestMethod.PUT)
     @ResponseBody
-    public Object update(@PathVariable String id) {
-        logger.info("update {}", id);
-        return Collections.emptyMap();
+    public Map<String, String> update(@RequestBody Map<String, String> todo,
+                                      @PathVariable String id) {
+        logger.info("update todo: {} {}", id, todo);
+        return todo;
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public HttpStatus destroy(@PathVariable String id) {
+        logger.info("destroy {}", id);
+        return HttpStatus.OK;
     }
 
 }
