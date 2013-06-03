49563d4ea98462a16497fc3249ece2928ac9c808
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2013-06-03 12:34:32 +0300

    H2 uses fail when starts as a server.

---
+++ src/test/java/cucumber/todo/hook/DbUnitHook.java
@@ -0,0 +1,17 @@
+package cucumber.todo.hook;
+
+import cucumber.api.java.Before;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.operation.DatabaseOperation;
+
+public class DbUnitHook {
+
+
+    @Before
+    public void setUpDbUnit() {
+        JdbcDatabaseTester databaseTester =
+                new JdbcDatabaseTester("org.h2.Driver", "jdbc:h2:tcp://localhost:9092/todos", "sa", "");
+        databaseTester.setTearDownOperation(DatabaseOperation.DELETE_ALL);
+    }
+
+}
